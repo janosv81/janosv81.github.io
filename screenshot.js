1c345361b6c1d7ac42df4559a0cb6aed004ff03b
Janos Virag <Janos.Virag@smartbox.com>
2015-06-24 16:38:16 +0100

    Implementing a way to add it to lastCompletedBuild page

--- screenshot.js
+++ screenshot.js
@@ -25,29 +25,62 @@ var appended;
     var testcaseName = testcaseArray[testcaseArray.size() - 1];
     var testcasePackage = urlparts[1].replace(/\//g, '.');
     var pngurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/smartboxfr_' + testcaseName + '.png';
-    logurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/sbx-test-log.log'
-    
-	link = document.createElement('a');
+    var logurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/sbx-test-log.log'
+    link = document.createElement('a');
     link.setAttribute('href', logurl);
     link.innerHTML = '  [LOG]';
     $(element).append(link);
-	
     link = document.createElement('a');
     link.setAttribute('href', pngurl);
     link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
     link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
     link.innerHTML = '     [SCREENSHOT]';
     $(element).append(link);
-	
     placeholder = document.createElement('img');
     placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
     placeholder.setAttribute('id', 'place-holder-' + index);
-    placeholder.setAttribute('style', 'zindex: 100; position: absolute; max-height: 350px;max-width: 350px;');
+    placeholder.setAttribute('style', 'zindex: 100; position: absolute; top: 350px; left: 350px; max-height: 350px;max-width: 350px;');
     $(element).append(placeholder);
+  }
+  var appendScreenshotCell = function (index, element) {
+    elem = $(element).find('a:nth-child(3)');
+    var url = elem.get(0).getAttribute('href');
+    var urlparts = url.split('/testngreports/');
+    var testcaseArray = urlparts[1].split('/');
+    var testcaseName = testcaseArray[testcaseArray.size() - 1];
+    var testcasePackage = urlparts[1].replace(/\//g, '.');
+    var pngurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/smartboxfr_' + testcaseName + '.png';
+    var logurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/sbx-test-log.log'
+    link = document.createElement('a');
+    link.setAttribute('href', logurl);
+    link.innerHTML = '[LOG]';
+    cell = document.createElement('td');
+    cell.setAttribute('class', 'log');
+    $(element).find('td:first').after(cell);
+    $(element).find('td.log').append(link);
+    placeholder = document.createElement('img');
+    placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+    placeholder.setAttribute('id', 'place-holder-' + index);
+    placeholder.setAttribute('style', 'zindex: 100; position: absolute; top: 350px; left: 350px; max-height: 350px;max-width: 350px;');
+    link = document.createElement('a');
+    link.setAttribute('href', pngurl);
+    link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
+    link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
+    link.innerHTML = '[SCREENSHOT]';
+    cell = document.createElement('td');
+    cell.setAttribute('class', 'screen');
+    $(element).find('td:first').after(cell);
+    $(element).find('td.screen').append(link, placeholder);
   }
   loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function () {
     if (appended != true) {
-      $('td#main-panel table tr:nth-child(5) ol li').each(appendScreenshotLink);
+      if (window.location.pathname.match(/.*lastCompletedBuild.*$/))
+      {
+        $('table#fail-tbl tbody tr').each(appendScreenshotCell);
+      } 
+      else {
+        $('td#main-panel table tr:nth-child(5) ol li').each(appendScreenshotLink);
+      }
     }
     appended = true;
   });
