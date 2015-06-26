4cf8db13600bacf01a9d5041fc51f55f225b6482
Janos Virag <Janos.Virag@smartbox.com>
2015-06-26 11:12:11 +0100

    Skipping non existing screenshots.
    Improvied CSS selector to find the table.

--- screenshot.js
+++ screenshot.js
@@ -24,23 +24,43 @@ var appended;
     var testcaseArray = urlparts[1].split('/');
     var testcaseName = testcaseArray[testcaseArray.size() - 1];
     var testcasePackage = urlparts[1].replace(/\//g, '.');
-    var pngurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/smartboxfr_' + testcaseName + '.png';
+    var pngurl = null;
+    var pngurl1 = urlparts[0] + '/artifact/reports/' + testcasePackage + '/smartboxfr_' + testcaseName + '.png';
+    var pngurl2 = urlparts[0] + '/artifact/reports/' + testcasePackage + '/' + testcaseName + '.png';
+    $.ajax(pngurl1,{type: "HEAD"}).done(function () {
+      pngurl = pngurl1;
+      link = document.createElement('a');
+      link.setAttribute('href', pngurl);
+      link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
+      link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
+      link.innerHTML = '     [SCREENSHOT]';
+      $(element).append(link);
+      placeholder = document.createElement('img');
+      placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+      placeholder.setAttribute('id', 'place-holder-' + index);
+      placeholder.setAttribute('style', 'zindex: 100; position: fixed; left: 20px; top: 100px; max-height: 600px;max-width: 800px;');
+      $(element).append(placeholder);
+    }).fail(function () {
+      $.ajax(pngurl2,{type: "HEAD"}).done(function () {
+        pngurl = pngurl2;
+        link = document.createElement('a');
+        link.setAttribute('href', pngurl);
+        link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
+        link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
+        link.innerHTML = '     [SCREENSHOT]';
+        $(element).append(link);
+        placeholder = document.createElement('img');
+        placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+        placeholder.setAttribute('id', 'place-holder-' + index);
+        placeholder.setAttribute('style', 'zindex: 100; position: fixed; left: 20px; top: 100px; max-height: 600px;max-width: 800px;');
+        $(element).append(placeholder);
+      })
+    });
     var logurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/sbx-test-log.log'
     link = document.createElement('a');
     link.setAttribute('href', logurl);
     link.innerHTML = '  [LOG]';
     $(element).append(link);
-    link = document.createElement('a');
-    link.setAttribute('href', pngurl);
-    link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
-    link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
-    link.innerHTML = '     [SCREENSHOT]';
-    $(element).append(link);
-    placeholder = document.createElement('img');
-    placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
-    placeholder.setAttribute('id', 'place-holder-' + index);
-    placeholder.setAttribute('style', "zindex: 100; position: fixed; left: 20px; top: 100px; max-height: 600px;max-width: 800px;");
-    $(element).append(placeholder);
   }
   var appendScreenshotCell = function (index, element) {
     elem = $(element).find('a:nth-child(3)');
@@ -49,7 +69,9 @@ var appended;
     var testcaseArray = urlparts[1].split('/');
     var testcaseName = testcaseArray[testcaseArray.size() - 1];
     var testcasePackage = urlparts[1].replace(/\//g, '.');
-    var pngurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/smartboxfr_' + testcaseName + '.png';
+    var pngurl = null;
+    var pngurl1 = urlparts[0] + '/artifact/reports/' + testcasePackage + '/smartboxfr_' + testcaseName + '.png';
+    var pngurl2 = urlparts[0] + '/artifact/reports/' + testcasePackage + '/' + testcaseName + '.png';
     var logurl = urlparts[0] + '/artifact/reports/' + testcasePackage + '/sbx-test-log.log'
     link = document.createElement('a');
     link.setAttribute('href', logurl);
@@ -58,19 +80,42 @@ var appended;
     cell.setAttribute('class', 'log');
     $(element).find('td:first').after(cell);
     $(element).find('td.log').append(link);
-    placeholder = document.createElement('img');
-    placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
-    placeholder.setAttribute('id', 'place-holder-' + index);
-    placeholder.setAttribute('style', "zindex: 100; position: fixed; left: 20px; top: 100px; max-height: 600px;max-width: 800px;");
-    link = document.createElement('a');
-    link.setAttribute('href', pngurl);
-    link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
-    link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
-    link.innerHTML = '[SCREENSHOT]';
-    cell = document.createElement('td');
-    cell.setAttribute('class', 'screen');
-    $(element).find('td:first').after(cell);
-    $(element).find('td.screen').append(link, placeholder);
+    $.ajax(pngurl1,{type: "HEAD"}).done(function () {
+      pngurl = pngurl1;
+      placeholder = document.createElement('img');
+      placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+      placeholder.setAttribute('id', 'place-holder-' + index);
+      placeholder.setAttribute('style', 'zindex: 100; position: fixed; left: 20px; top: 100px; max-height: 600px;max-width: 800px;');
+      link = document.createElement('a');
+      link.setAttribute('href', pngurl);
+      link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
+      link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
+      link.innerHTML = '[SCREENSHOT]';
+      cell = document.createElement('td');
+      cell.setAttribute('class', 'screen');
+      $(element).find('td:first').after(cell);
+      $(element).find('td.screen').append(link, placeholder);
+    }).fail(function () {
+      $.ajax(pngurl2,{type: "HEAD"}).done(function () {
+        pngurl = pngurl2;
+        placeholder = document.createElement('img');
+        placeholder.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+        placeholder.setAttribute('id', 'place-holder-' + index);
+        placeholder.setAttribute('style', 'zindex: 100; position: fixed; left: 20px; top: 100px; max-height: 600px;max-width: 800px;');
+        link = document.createElement('a');
+        link.setAttribute('href', pngurl);
+        link.setAttribute('onmouseover', 'document.getElementById(\'place-holder-' + index + '\').src=\'' + pngurl + '\';');
+        link.setAttribute('onmouseout', 'document.getElementById(\'place-holder-' + index + '\').src=\'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png\';');
+        link.innerHTML = '[SCREENSHOT]';
+        cell = document.createElement('td');
+        cell.setAttribute('class', 'screen');
+        $(element).find('td:first').after(cell);
+        $(element).find('td.screen').append(link, placeholder);
+      }).fail(function () {
+		          cell = document.createElement('td');
+        cell.setAttribute('class', 'screen');
+	  $(element).find('td:first').after(cell); })
+    });
   }
   loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function () {
     if (appended != true) {
@@ -79,7 +124,7 @@ var appended;
         $('table#fail-tbl tbody tr').each(appendScreenshotCell);
       } 
       else {
-        $('td#main-panel table tr:nth-child(5) ol li').each(appendScreenshotLink);
+        $('a[href="testngreports"]').parent().find('ol:first li').each(appendScreenshotLink);
       }
     }
     appended = true;
