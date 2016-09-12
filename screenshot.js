09227bffb21f83bc8c9131d8f5b53175a9035fc1
janosv81 <virag.janos@gmail.com>
2016-09-12 13:40:39 +0100

    Fixing invalid regexp flag

--- screenshot.js
+++ screenshot.js
@@ -22,7 +22,7 @@ var appended;
     var url = element.firstChild.getAttribute('href');
     $.ajax(url,{type: "GET"}).done(function( data ) {
     if ( console && console.log ) {
-      var match = data.match(/Message: <\/b>.*?<\/p>/gs);
+      var match = data.match(/Message: <\/b>.*?<\/p>/gm);
       console.log( "Sample of data:",  match );
       $(element).after("<b><p>"+match);
     }
